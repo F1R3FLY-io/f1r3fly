03fe1330af15bcbcb4986494f61fc01e70cabd1c
Paul Snively <psnively@mac.com>
2024-01-08 13:49:02 -0500

    Added missing CompiledRholangSource.scala and fixed .gitignore to only ignore  at the root of the tree.

---
+++ rholang/src/main/scala/coop/rchain/rholang/build/CompiledRholangSource.scala
@@ -0,0 +1,66 @@
+package coop.rchain.rholang.build
+import coop.rchain.models.NormalizerEnv.ToEnvMap
+import coop.rchain.models.{NormalizerEnv, Par}
+import coop.rchain.rholang.interpreter.compiler.Compiler
+import monix.eval.Coeval
+import shapeless.HNil
+
+import scala.io.Source
+
+/** TODO: Currently all calls to this class use empty environment. See [[NormalizerEnv]]. */
+abstract class CompiledRholangSource[Env](val code: String, val normalizerEnv: NormalizerEnv[Env])(
+    implicit ev: ToEnvMap[Env]
+) {
+  val path: String
+  val term: Par = Compiler[Coeval].sourceToADT(code, normalizerEnv.toEnv).value()
+  final def env = normalizerEnv.toEnv
+}
+
+object CompiledRholangSource {
+  def loadSource(classpath: String) = {
+    val fileContent = Source.fromResource(classpath).mkString
+    s"""//Loaded from resource file <<$classpath>>
+       #$fileContent
+       #""".stripMargin('#')
+  }
+
+  def apply(classpath: String): CompiledRholangSource[HNil] = apply(classpath, NormalizerEnv.Empty)
+
+  def apply[Env](classpath: String, env: NormalizerEnv[Env])(
+      implicit ev: ToEnvMap[Env]
+  ): CompiledRholangSource[Env] = new CompiledRholangSource[Env](loadSource(classpath), env) {
+    override val path: String = classpath
+  }
+}
+
+/**
+  * Loads code from a resource file while doing macro substitution with the provided values.
+  * The macros have the format $$macro$$
+  * @param classpath
+  * @param env a sequence of pairs macro -> value
+  * @return
+  */
+abstract class CompiledRholangTemplate[Env](
+    classpath: String,
+    normalizerEnv0: NormalizerEnv[Env],
+    env: (String, Any)*
+)(implicit ev: ToEnvMap[Env])
+    extends CompiledRholangSource[Env](
+      CompiledRholangTemplate.loadTemplate(classpath, env),
+      normalizerEnv0
+    ) {
+  override val path: String = classpath
+}
+
+object CompiledRholangTemplate {
+  def loadTemplate(classpath: String, macros: Seq[(String, Any)]) = {
+    val originalContent = Source.fromResource(classpath).mkString
+    val finalContent = macros.foldLeft(originalContent) {
+      case (content, (name, value)) => content.replace(s"""$$$$$name$$$$""", value.toString)
+    }
+
+    s"""//Loaded from resource file <<$classpath>>
+       #$finalContent
+       #""".stripMargin('#')
+  }
+}
