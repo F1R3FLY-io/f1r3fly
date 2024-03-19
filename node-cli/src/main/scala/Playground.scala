c08da181023388e641dba0bbae575fe2ef9d976a
steve preston <stevepreston747@gmail.com>
2024-03-19 16:59:38 -0700

    NodeCliPlayground to propose after deployData

--- node-cli/src/main/scala/Playground.scala
+++ node-cli/src/main/scala/Playground.scala
@@ -1,115 +1,114 @@
 import coop.rchain.casper.util.comm._
 import java.nio.file.Paths
 import monix.eval.Task
+import coop.rchain.crypto.PrivateKey
+import com.google.protobuf.ByteString
+import monix.execution.Scheduler.Implicits.global
+import coop.rchain.casper.protocol.DeployData
+import java.nio.file.Files
+import java.nio.charset.StandardCharsets
+import coop.rchain.crypto.signatures.Secp256k1
+import coop.rchain.crypto.signatures.Signed
 
-class Playground {
+class NodeCliPlayground {
   val rhoContractPath = "./rholang/examples/tut-registry.rho"
-  val vpk             = "aebb63dc0d50e4dd29ddd94fb52103bfe0dc4941fa0c2c8a9082a191af35ffa1"
+  val vpkHex          = "aebb63dc0d50e4dd29ddd94fb52103bfe0dc4941fa0c2c8a9082a191af35ffa1"
+  val vpkBytes        = javax.xml.bind.DatatypeConverter.parseHexBinary(vpkHex)
   val host            = "127.0.0.1"
   val port            = 40402
 
-  def go(): Unit =
-    printCurrentDirectory()
+  def go(): Unit = {
+    implicit val deployServiceClient: GrpcDeployService[Task] =
+      new GrpcDeployService[Task](
+        host,
+        port,
+        16 * 1024 * 1024
+      )
 
-  // Clients for executing gRPC calls on remote RNode instance
-  implicit val deployServiceClient: GrpcDeployService[Task] =
-    new GrpcDeployService[Task](
-      host,
-      port,
-      16 * 1024 * 1024
+    val rhoContractCode = loadFileContent(rhoContractPath)
+
+    val d = DeployData(
+      term = rhoContractCode,
+      timestamp = 0,
+      phloPrice = 1,
+      phloLimit = 1000000,
+      shardId = "root",
+      validAfterBlockNumber = 0
     )
-  implicit val proposeServiceClient: GrpcProposeService[Task] =
-    new GrpcProposeService[Task](
-      host,
-      port,
-      16 * 1024 * 1024
-    )
-  // val rhoContractCode = loadFileContent(rhoContractPath)
 
-  // val ddp = DeployDataProto(
-  //   term = rhoContractCode,
-  //   timestamp = 0,
-  //   phloPrice = 1,
-  //   phloLimit = 1000000,
-  //   shardId = "root",
-  //   validAfterBlockNumber = 0
-  // )
+    val deployTask =
+      DeployService[Task].deploy(Signed(d, Secp256k1, PrivateKey(ByteString.copyFrom(vpkBytes))))
 
-  // val signedDeploy = signDeploy(ECPrivateKey(vpk), ddp)
+    deployTask.runAsync {
+      case Right(res) => {
 
-  // // Deploy
-  // val deployVolumeContract: Future[Unit] = for {
-  //   deployResponse <- DeployService.doDeploy(signedDeploy).toScala
-  //   deployResult <- if (deployResponse.hasError) {
-  //                    Future.failed(new Exception(deployResponse.getError))
-  //                  } else {
-  //                    Future.successful(deployResponse.getResult)
-  //                  }
-  //   deployId = deployResult.substring(deployResult.indexOf("DeployId is: ") + 13)
-  //   proposeResponse <- proposeService
-  //                       .propose(ProposeQuery.newBuilder().setIsAsync(false).build())
-  //                       .toScala
-  //   _ <- if (proposeResponse.hasError) {
-  //         Future.failed(new Exception(proposeResponse.getError))
-  //       } else {
-  //         Future.successful(())
-  //       }
-  //   b64 = ByteString.copyFrom(Hex.decode(deployId))
-  //   findResponse <- deployService
-  //                    .findDeploy(FindDeployQuery.newBuilder().setDeployId(b64).build())
-  //                    .toScala
-  //   blockHash <- if (findResponse.hasError) {
-  //                 Future.failed(new Exception(findResponse.getError))
-  //               } else {
-  //                 Future.successful(findResponse.getBlockInfo.getBlockHash)
-  //               }
-  //   isFinalizedResponse <- deployService
-  //                           .isFinalized(IsFinalizedQuery.newBuilder().setHash(blockHash).build())
-  //                           .toScala
-  //   _ <- if (isFinalizedResponse.hasError || !isFinalizedResponse.getIsFinalized) {
-  //         Future.failed(new Exception(isFinalizedResponse.getError))
-  //       } else {
-  //         Future.successful(())
-  //       }
-  // } yield ()
+        res match {
+          case Right(str) => {
+            println(s"Deploy $str")
 
-  // deployVolumeContract.onComplete {
-  //   case Success(_)         => println("Deployment successful")
-  //   case Failure(exception) => println(s"Deployment failed: ${exception.getMessage}")
-  // }
+            val deployIdPatternFull = "DeployId is: (.+)".r
+            val deployId            = deployIdPatternFull.findFirstMatchIn(str).map(_.group(1))
 
-  // def signDeploy(privKey: ECPrivateKey, deploy: DeployDataProto): DeployDataProto = {
-  //   // Take a projection of only the fields used to validate the signature
-  //   val projection = DeployDataProto(
-  //     term = deploy.term,
-  //     timestamp = deploy.timestamp,
-  //     phloPrice = deploy.phloPrice,
-  //     phloLimit = deploy.phloLimit,
-  //     validAfterBlockNumber = deploy.validAfterBlockNumber,
-  //     shardId = deploy.shardId
-  //   )
-  //   val serialized = ByteVector(projection.toByteArray)
-  //   val deployer   = privKey.publicKey.decompressed
-  //   val digest     = ByteVector(Blake2b256.hash(serialized.toArray))
-  //   val signed     = privKey.sign(digest)
-  //   projection.copy(
-  //     sigAlgorithm = "secp256k1",
-  //     sig = ByteString.copyFrom(signed.bytes.toArray),
-  //     deployer = ByteString.copyFrom(deployer.decompressedBytes.toArray)
-  //   )
-  // }
+            deployId match {
+              case Some(id) => {
+                propose()
+                useDeployId(id)
+              }
+              case None =>
+                println("Deploy ID not found in the output.")
+            }
+          }
+          case Left(errors) => errors.foreach(println)
+        }
+
+      }
+      case Left(err) => println(s"Deploy failed: $err")
+    }
+
+  }
+
+  def propose(): Boolean = {
+    implicit val proposeServiceClient: GrpcProposeService[Task] =
+      new GrpcProposeService[Task](
+        host,
+        port,
+        16 * 1024 * 1024
+      )
+
+    val proposeTask =
+      ProposeService[Task].propose(true)
+
+    proposeTask.runAsync {
+      case Right(res) => {
+
+        res match {
+          case Right(str) => {
+            println(s"Propose $str")
+          }
+          case Left(errors) => errors.foreach(println)
+        }
+
+      }
+      case Left(err) => println(s"Propose failed: $err")
+    }
+
+    true
+  }
+
+  def useDeployId(deployId: String): Unit =
+    println(s"Using deploy ID: $deployId")
+
+  def loadFileContent(filePath: String): String =
+    new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
 
   def printCurrentDirectory(): Unit = {
     val currentPath = Paths.get("").toAbsolutePath.toString
     println(s"Current working directory: $currentPath")
   }
 
-  // def loadFileContent(filePath: String): String =
-  //   new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
-
 }
 
-object PlaygroundApp extends App {
-  val playground = new Playground()
+object NodeCliPlaygroundApp extends App {
+  val playground = new NodeCliPlayground()
   playground.go()
 }
