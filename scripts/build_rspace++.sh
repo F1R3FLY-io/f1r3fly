768af1eefde0fdeb2c36bb285ecaaeaeacbfd529
steve preston <stevepreston747@gmail.com>
2024-05-02 16:17:04 -0700

    fixing build script

--- scripts/build_rspace++.sh
+++ scripts/build_rspace++.sh
@@ -4,6 +4,7 @@ cd rspace++/
 cargo build --profile dev -p rspace_plus_plus_rhotypes
 
 # This is needed for debugging through VS Code 'Metals' extension on M2
+ARCH_TYPE=$(uname -m)
 if [ "$ARCH_TYPE" == "arm64" ]; then
 	cargo build --profile dev --target x86_64-apple-darwin -p rspace_plus_plus_rhotypes
 fi
