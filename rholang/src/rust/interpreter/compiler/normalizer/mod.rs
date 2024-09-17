a2a613fd5e79d794c1a32cabf8b9aea0b48f95c5
Nazar Yanovets <nyanovet8@gmail.com>
2024-09-17 17:13:59 +0300

    comment several mods

--- rholang/src/rust/interpreter/compiler/normalizer/mod.rs
+++ rholang/src/rust/interpreter/compiler/normalizer/mod.rs
@@ -1,7 +1,7 @@
 pub mod exports;
 pub mod bool_normalize_matcher;
-pub mod collection_normalize_matcher;
+//pub mod collection_normalize_matcher;
 pub mod ground_normalize_matcher;
 pub mod parser;
 pub mod processes;
-mod name_normalize_matcher;
+//mod name_normalize_matcher;
