44be18506986adbd4dfe36db8f7591eba47ef950
Nazar Yanovets <nyanovet8@gmail.com>
2024-09-26 19:18:53 +0300

    update tree-sitter files, based on comments in Rholang code

--- rholang/clean_tree_sitter_generated_files.sh
+++ rholang/clean_tree_sitter_generated_files.sh
@@ -6,19 +6,19 @@
 
 
 
-# Cleaning up the src/main/tree_sitter directory, leaving only grammar.js, GPT-BNFC-to-Tree-Sitter.md, and the tests folder.
+# Cleaning up the src/main/tree_sitter directory, leaving only needed files for Rust, Parser and tests.
 DIR="src/main/tree_sitter"
 
-find "$DIR" -mindepth 1 ! -name "grammar.js" ! -name "GPT-BNFC-to-Tree-Sitter.md" ! -name "tests" ! -path "$DIR/tests/*" ! -name "test" ! -path "$DIR/test/*" -exec rm -rf {} +
+find "$DIR" -mindepth 1 ! -name "grammar.js" ! -name "Cargo.lock" ! -name "Cargo.toml"  ! -name "GPT-BNFC-to-Tree-Sitter.md" ! -name "tests" ! -path "$DIR/tests/*" ! -name "test" ! -path "$DIR/test/*" ! -name "bindings" ! -path "$DIR/bindings/*" ! -name "src" ! -path "$DIR/src/*" -exec rm -rf {} +
 
 # Removing the target folder in the root directory if it exists.
-TARGET_DIR="target"
-
-if [ -d "$TARGET_DIR" ]; then
-    rm -rf "$TARGET_DIR"
-    echo "The $TARGET_DIR folder has been successfully removed."
-else
-    echo "The $TARGET_DIR folder was not found."
-fi
+#TARGET_DIR="target"
+#
+#if [ -d "$TARGET_DIR" ]; then
+#    rm -rf "$TARGET_DIR"
+#    echo "The $TARGET_DIR folder has been successfully removed."
+#else
+#    echo "The $TARGET_DIR folder was not found."
+#fi
 
 echo "Cleanup completed."
