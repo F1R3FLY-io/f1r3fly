a69e024a1fa60d2b7775b0831682447fea37a25c
MarcinR-DevBrother <marcin.r@devbrother.com>
2024-09-26 16:32:41 +0200

    Update tree-sitter to 0.23

--- rholang/src/rust/interpreter/compiler/normalizer/parser.rs
+++ rholang/src/rust/interpreter/compiler/normalizer/parser.rs
@@ -1,9 +1,10 @@
 use tree_sitter::{Parser, Tree};
-use tree_sitter_rholang::language;
 
 pub fn parse_rholang_code(code: &str) -> Tree {
-  let mut parser = Parser::new();
-  parser.set_language(&language()).expect("Error loading Rholang grammar");
-  println!("Language {:?}", parser.language());
-  parser.parse(code, None).expect("Failed to parse code")
+    let mut parser = Parser::new();
+    parser
+        .set_language(&tree_sitter_rholang::LANGUAGE.into())
+        .expect("Error loading Rholang grammar");
+    println!("Language {:?}", parser.language());
+    parser.parse(code, None).expect("Failed to parse code")
 }
