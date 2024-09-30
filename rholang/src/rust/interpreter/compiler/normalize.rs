71db38e8c282400f9abe5f81add154e01b0f291c
Nazar Yanovets <nyanovet8@gmail.com>
2024-09-30 17:52:03 +0300

    updated unary and binary cases base on a new grammar

--- rholang/src/rust/interpreter/compiler/normalize.rs
+++ rholang/src/rust/interpreter/compiler/normalize.rs
@@ -103,7 +103,36 @@ pub fn normalize_match(
       free_map: right_result.free_map,
     })
   }
+
+  //help func for unary cases:
+  fn handle_unary_expression(
+    p_node: Node,
+    input: ProcVisitInputs,
+    source_code: &[u8],
+    constructor: Box<dyn UnaryExpr>,
+  ) -> Result<ProcVisitOutputs, Box<dyn Error>> {
+    let sub_proc_node = p_node.child_by_field_name("proc")
+      .or(p_node.child_by_field_name("left"))
+      .ok_or("Expected a child node for unary expression but found None")?;
+
+    unary_exp(sub_proc_node, input, source_code, constructor)
+  }
+
+
+  //help func for binary cases:
+  fn handle_binary_expression(
+    p_node: Node,
+    input: ProcVisitInputs,
+    source_code: &[u8],
+    constructor: Box<dyn BinaryExpr>,
+  ) -> Result<ProcVisitOutputs, Box<dyn Error>> {
+    let left_node = p_node.child_by_field_name("left").ok_or("Expected a left node but found None")?;
+    let right_node = p_node.child_by_field_name("right").ok_or("Expected a right node but found None")?;
+    binary_exp(left_node, right_node, input, source_code, constructor)
+  }
+
   println!("Normalizing node kind: {}", p_node.kind());
+
   match p_node.kind() {
     "bundle" => {
       println!("Found a bundle node, calling normalize_p_bundle");
@@ -118,135 +147,26 @@ pub fn normalize_match(
     "block" => normalize_match(p_node.child_by_field_name("body").unwrap(), input, source_code),
 
     //unary
-    "not" => unary_exp(
-      p_node.child(0).unwrap(),
-      input,
-      source_code,
-      Box::new(ENot::default()),
-    ),
-    "neg" => unary_exp(
-      p_node.child(0).unwrap(),
-      input,
-      source_code,
-      Box::new(ENeg::default()),
-    ),
+    "not" => handle_unary_expression(p_node, input, source_code, Box::new(ENot::default())),
+    "neg" => handle_unary_expression(p_node, input, source_code, Box::new(ENeg::default())),
 
     //binary
-    "mult" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EMult::default()),
-    ),
-    "div" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EDiv::default()),
-    ),
-    "mod" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EMod::default()),
-    ),
-    "percent_percent" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EPercentPercent::default()),
-    ),
-    "add" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EPlus::default()),
-    ),
-    "minus" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EMinus::default()),
-    ),
-    "plus_plus" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EPlusPlus::default()),
-    ),
-    "minus_minus" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EMinusMinus::default()),
-    ),
-
-    "lt" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(ELt::default()),
-    ),
-    "lte" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(ELte::default()),
-    ),
-    "gt" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EGt::default()),
-    ),
-    "gte" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EGte::default()),
-    ),
-
-    "eq" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EEq::default()),
-    ),
-    "neq" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(ENeq::default()),
-    ),
-
-    "and" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EAnd::default()),
-    ),
-    "or" => binary_exp(
-      p_node.child(0).unwrap(),
-      p_node.child(1).unwrap(),
-      input,
-      source_code,
-      Box::new(EOr::default()),
-    ),
+    "mult" => handle_binary_expression(p_node, input, source_code, Box::new(EMult::default())),
+    "div" => handle_binary_expression(p_node, input, source_code, Box::new(EDiv::default())),
+    "mod" => handle_binary_expression(p_node, input, source_code, Box::new(EMod::default())),
+    "percent_percent" => handle_binary_expression(p_node, input, source_code, Box::new(EPercentPercent::default())),
+    "add" => handle_binary_expression(p_node, input, source_code, Box::new(EPlus::default())),
+    "minus" => handle_binary_expression(p_node, input, source_code, Box::new(EMinus::default())),
+    "plus_plus" => handle_binary_expression(p_node, input, source_code, Box::new(EPlusPlus::default())),
+    "minus_minus" => handle_binary_expression(p_node, input, source_code, Box::new(EMinusMinus::default())),
+    "lt" => handle_binary_expression(p_node, input, source_code, Box::new(ELt::default())),
+    "lte" => handle_binary_expression(p_node, input, source_code, Box::new(ELte::default())),
+    "gt" => handle_binary_expression(p_node, input, source_code, Box::new(EGt::default())),
+    "gte" => handle_binary_expression(p_node, input, source_code, Box::new(EGte::default())),
+    "eq" => handle_binary_expression(p_node, input, source_code, Box::new(EEq::default())),
+    "neq" => handle_binary_expression(p_node, input, source_code, Box::new(ENeq::default())),
+    "and" => handle_binary_expression(p_node, input, source_code, Box::new(EAnd::default())),
+    "or" => handle_binary_expression(p_node, input, source_code, Box::new(EOr::default())),
     _ => Err(format!("Compilation of construct not yet supported.: {}", p_node.kind()).into()),
   }
 }
