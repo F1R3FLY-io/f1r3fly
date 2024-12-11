659ee9e8789a964ed350bda69f63850bf4459c08
Nazar Yanovets <nazar.y@devbrother.com>
2024-12-11 19:41:57 +0200

    - implemented ed25519; - implemented secp256k1;

--- crypto/src/rust/signatures/ed25519.rs
+++ crypto/src/rust/signatures/ed25519.rs
@@ -1,38 +1,138 @@
 use super::signatures_alg::SignaturesAlg;
+use crate::rust::{private_key::PrivateKey, public_key::PublicKey};
+use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
+use rand::rngs::OsRng;
 
 // See crypto/src/main/scala/coop/rchain/crypto/signatures/Ed25519.scala
 pub struct Ed25519;
 
 impl SignaturesAlg for Ed25519 {
     fn verify(&self, data: &[u8], signature: &[u8], pub_key: Vec<u8>) -> bool {
-        todo!()
+        let public_key = match parse_public_key(&pub_key) {
+            Ok(key) => key,
+            Err(err) => {
+                eprintln!("{}", err);
+                return false;
+            }
+        };
+
+        let signature = match parse_signature(signature) {
+            Ok(sig) => sig,
+            Err(err) => {
+                eprintln!("{}", err);
+                return false;
+            }
+        };
+
+        public_key.verify(data, &signature).is_ok()
     }
 
     fn sign(&self, data: &[u8], sec: &[u8]) -> Vec<u8> {
-        todo!()
+        let signing_key = parse_signing_key(sec).expect("Secret key must be 32 bytes");
+        let signature = signing_key.sign(data);
+        signature.to_bytes().to_vec()
     }
 
-    fn to_public(
-        &self,
-        sec: &crate::rust::private_key::PrivateKey,
-    ) -> crate::rust::public_key::PublicKey {
-        todo!()
+    fn to_public(&self, sec: &PrivateKey) -> PublicKey {
+        let signing_key = parse_signing_key(&sec.bytes).expect("Secret key must be 32 bytes");
+        let public_key = signing_key.verifying_key();
+
+        PublicKey {
+            bytes: public_key.to_bytes().to_vec(),
+        }
     }
 
-    fn new_key_pair(
-        &self,
-    ) -> (
-        crate::rust::private_key::PrivateKey,
-        crate::rust::public_key::PublicKey,
-    ) {
-        todo!()
+    fn new_key_pair(&self) -> (PrivateKey, PublicKey) {
+        let mut csprng = OsRng;
+
+        let signing_key = SigningKey::generate(&mut csprng);
+        let public_key = signing_key.verifying_key();
+
+        let private_key = PrivateKey {
+            bytes: signing_key.to_bytes().to_vec(),
+        };
+        let public_key = PublicKey {
+            bytes: public_key.to_bytes().to_vec(),
+        };
+
+        (private_key, public_key)
     }
 
     fn name(&self) -> String {
-        todo!()
+        "ed25519".to_string()
     }
 
     fn sig_length(&self) -> usize {
-        todo!()
+        64
+    }
+}
+
+fn parse_public_key(pub_key: &[u8]) -> Result<VerifyingKey, &'static str> {
+    pub_key
+        .try_into()
+        .map_err(|_| "Public key must be 32 bytes")
+        .and_then(|key| VerifyingKey::from_bytes(key).map_err(|_| "Invalid public key"))
+}
+
+fn parse_signature(signature: &[u8]) -> Result<Signature, &'static str> {
+    signature
+        .try_into()
+        .map_err(|_| "Signature must be 64 bytes")
+        .map(Signature::from_bytes)
+}
+
+fn parse_signing_key(secret: &[u8]) -> Result<SigningKey, &'static str> {
+    secret
+        .try_into()
+        .map_err(|_| "Secret key must be 32 bytes")
+        .map(SigningKey::from_bytes)
+}
+
+// crypto/src/test/scala/coop/rchain/crypto/signatures/Ed25519Test.scala
+#[cfg(test)]
+mod tests {
+    use super::Ed25519;
+    use crate::rust::signatures::signatures_alg::SignaturesAlg;
+    use crate::rust::{private_key::PrivateKey, public_key::PublicKey as RPublicKey};
+    use hex::decode;
+
+    #[test]
+    fn computes_public_key_from_secret_key() {
+        let ed25519 = Ed25519;
+
+        let sec =
+            decode("b18e1d0045995ec3d010c387ccfeb984d783af8fbb0f40fa7db126d889f6dadd").unwrap();
+        let expected_pub = "77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb";
+
+        let private_key = PrivateKey { bytes: sec };
+        let public_key = ed25519.to_public(&private_key);
+
+        assert_eq!(hex::encode(public_key.bytes), expected_pub);
+    }
+
+    #[test]
+    fn verifies_the_given_signature() {
+        let ed25519 = Ed25519;
+
+        let data = decode("916c7d1d268fc0e77c1bef238432573c39be577bbea0998936add2b50a653171ce18a542b0b7f96c1691a3be6031522894a8634183eda38798a0c5d5d79fbd01dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460376d7f3ac22ff372c18f613f2ae2e856af40").unwrap();
+        let sig = decode("6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509").unwrap();
+        let pub_key =
+            decode("77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb").unwrap();
+
+        assert!(ed25519.verify(&data, &sig, pub_key));
+    }
+
+    #[test]
+    fn creates_a_signature() {
+        let ed25519 = Ed25519;
+
+        let data = decode("916c7d1d268fc0e77c1bef238432573c39be577bbea0998936add2b50a653171ce18a542b0b7f96c1691a3be6031522894a8634183eda38798a0c5d5d79fbd01dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460376d7f3ac22ff372c18f613f2ae2e856af40").unwrap();
+        let expected_sig = decode("6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509").unwrap();
+        let sec =
+            decode("b18e1d0045995ec3d010c387ccfeb984d783af8fbb0f40fa7db126d889f6dadd").unwrap();
+
+        let sig = ed25519.sign(&data, &sec);
+
+        assert_eq!(sig, expected_sig);
     }
 }
--- crypto/src/rust/signatures/secp256k1.rs
+++ crypto/src/rust/signatures/secp256k1.rs
@@ -1,32 +1,196 @@
-use crate::rust::{private_key::PrivateKey, public_key::PublicKey};
-
 use super::signatures_alg::SignaturesAlg;
+use crate::rust::{private_key::PrivateKey, public_key::PublicKey};
+use k256::ecdsa::signature::Verifier;
+use k256::ecdsa::VerifyingKey;
+use k256::{
+    ecdsa::{signature::Signer, Signature, SigningKey},
+    elliptic_curve::generic_array::GenericArray,
+};
+
+use typenum::U32;
+
+use k256::elliptic_curve::Field;
+use k256::elliptic_curve::{sec1::ToEncodedPoint, SecretKey};
+use rand::rngs::OsRng;
 
 // See crypto/src/main/scala/coop/rchain/crypto/signatures/Secp256k1.scala
 pub struct Secp256k1;
 
+impl Secp256k1 {
+    fn sec_key_verify(&self, seckey: &[u8]) -> bool {
+        seckey.len() == 32 && SigningKey::from_bytes(GenericArray::from_slice(seckey)).is_ok()
+    }
+}
+
 impl SignaturesAlg for Secp256k1 {
     fn verify(&self, data: &[u8], signature: &[u8], pub_key: Vec<u8>) -> bool {
-        todo!()
+        VerifyingKey::from_sec1_bytes(&pub_key)
+            .and_then(|vk| Signature::from_der(signature).map(|sig| vk.verify(data, &sig)))
+            .is_ok()
     }
 
     fn sign(&self, data: &[u8], sec: &[u8]) -> Vec<u8> {
-        todo!()
+        let key_bytes = GenericArray::clone_from_slice(sec);
+        let signing_key = SigningKey::from_bytes(&key_bytes).expect("Invalid private key");
+
+        let signature: Signature = signing_key.sign(data);
+        signature.to_der().as_bytes().to_vec()
     }
 
     fn to_public(&self, sec: &PrivateKey) -> PublicKey {
-        todo!()
+        let key_bytes: GenericArray<u8, U32> = GenericArray::clone_from_slice(&sec.bytes);
+        let secret_key: SecretKey<k256::Secp256k1> =
+            SecretKey::from_bytes(&key_bytes).expect("Invalid private key");
+
+        let public_key = secret_key.public_key();
+        let public_key_bytes = public_key.to_encoded_point(false).as_bytes().to_vec();
+
+        PublicKey {
+            bytes: public_key_bytes,
+        }
     }
 
     fn new_key_pair(&self) -> (PrivateKey, PublicKey) {
-        todo!()
+        let secret_key = SecretKey::<k256::Secp256k1>::random(&mut OsRng);
+        let raw_public_key = secret_key.public_key();
+
+        let private_key = PrivateKey {
+            bytes: secret_key.to_bytes().to_vec(),
+        };
+
+        let public_key = PublicKey {
+            bytes: raw_public_key.to_encoded_point(false).as_bytes().to_vec(),
+        };
+
+        (private_key, public_key)
     }
 
     fn name(&self) -> String {
-        todo!()
+        "secp256k1".to_string()
     }
 
     fn sig_length(&self) -> usize {
-        todo!()
+        32
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sha2::{Digest, Sha256};
+
+    //crypto/src/test/scala/coop/rchain/crypto/signatures/Secp256k1Spec.scala
+    #[test]
+    fn generate_valid_key_pair() {
+        let secp256k1 = Secp256k1;
+
+        let all_pairs_valid = (0..1000).all(|_| {
+            let (_private_key, _public_key) = secp256k1.new_key_pair();
+            true
+        });
+
+        assert!(
+            all_pairs_valid,
+            "Not all key pairs were created successfully"
+        );
+    }
+
+    //crypto/src/test/scala/coop/rchain/crypto/signatures/Secp256k1Test.scala
+    #[test]
+    fn verifies_the_given_secp256k1_signature_in_native_code_with_keypair() {
+        let secp256k1 = Secp256k1;
+        let (private_key, public_key) = secp256k1.new_key_pair();
+        let data = Sha256::digest(b"testing");
+
+        let signature = secp256k1.sign(&data, &private_key.bytes);
+        let is_valid = secp256k1.verify(&data, &signature, public_key.bytes);
+
+        assert!(is_valid, "Signature is not valid for the generated keypair");
+    }
+
+    /*
+    Something went wrong if I take string key and sig from Scala, I think that because different
+    libraries works in different style under the hood and can return different numbers of bytes for hashed data or have different implementation of signatures.
+    So, I'm generated test keys and sig with Rust 256k1 and generate_keys_and_signature_for_hashed_data()
+     */
+    #[test]
+    fn verifies_the_given_secp256k1_signature_in_native_code() {
+        let secp256k1 = Secp256k1;
+        let data = Sha256::digest(b"testing");
+        let public_key = hex::decode("0418a6b57c4aeee6c7e19e3ea25aa5bae270eca8580ee5e59c28921df743e416a316c55ed10c63b99a7c2705de0e0d3c52ad7f06144b7f6ed97d3a63b871ced6ff")
+        .expect("Failed to decode public key");
+        let signature = hex::decode("3045022100acb2ec6a831ef053de04b55ad26fb5288204969f6c8eaabb52d75f4942be293e02205016a682d3111e9d13b744362807c9039f455c5457dea240f6b442f341ece7bd")
+        .expect("Failed to decode signature");
+
+        let is_valid = secp256k1.verify(&data, &signature, public_key.clone());
+
+        assert!(is_valid, "Static signature verification failed");
+    }
+
+    #[test]
+    fn packaged_in_libsecp256k1_creates_an_ecdsa_signature() {
+        let secp256k1 = Secp256k1;
+        let data = Sha256::digest(b"testing");
+
+        let private_key =
+            hex::decode("54d4e576251b6c7b95af0349a5a31e583e982edeb9856a59bde14f6be5a302b3")
+                .expect("Failed to decode private key");
+
+        let expected_signature = hex::decode("3045022100acb2ec6a831ef053de04b55ad26fb5288204969f6c8eaabb52d75f4942be293e02205016a682d3111e9d13b744362807c9039f455c5457dea240f6b442f341ece7bd")
+      .expect("Failed to decode expected signature");
+
+        let generated_signature = secp256k1.sign(&data, &private_key);
+
+        assert_eq!(
+            hex::encode(generated_signature),
+            hex::encode(expected_signature),
+            "Generated signature does not match the expected signature"
+        );
+    }
+
+    #[test]
+    fn verify_returns_true_if_valid_false_if_invalid() {
+        let secp256k1 = Secp256k1;
+
+        let valid_private_key =
+            hex::decode("75468aa68961817b41d7fc2350ae705a7d773ea0d6609f5c25c2aee8e0adced6")
+                .expect("Failed to decode valid private key");
+
+        assert!(
+            secp256k1.sec_key_verify(&valid_private_key),
+            "Valid private key verification failed"
+        );
+    }
+
+    #[test]
+    fn computes_public_key_from_secret_key() {
+        let secp256k1 = Secp256k1;
+
+        let private_key =
+            hex::decode("54d4e576251b6c7b95af0349a5a31e583e982edeb9856a59bde14f6be5a302b3")
+                .expect("Failed to decode private key");
+        let expected_public_key = "0418a6b57c4aeee6c7e19e3ea25aa5bae270eca8580ee5e59c28921df743e416a316c55ed10c63b99a7c2705de0e0d3c52ad7f06144b7f6ed97d3a63b871ced6ff";
+
+        let computed_public_key = secp256k1.to_public(&PrivateKey { bytes: private_key });
+
+        assert_eq!(
+            hex::encode(computed_public_key.bytes).to_uppercase(),
+            expected_public_key.to_uppercase(),
+            "Computed public key does not match the expected public key"
+        );
+    }
+
+    // #[test]
+    // fn generate_keys_and_signature_for_hashed_data() {
+    //   let secp256k1 = Secp256k1;
+    //
+    //   let (private_key, public_key) = secp256k1.new_key_pair();
+    //   let data = Sha256::digest(b"testing");
+    //
+    //   let signature = secp256k1.sign(&data, &private_key.bytes);
+    //   println!("Private key: {}", hex::encode(&private_key.bytes));
+    //   println!("Public key: {}", hex::encode(&public_key.bytes));
+    //   println!("Signature: {}", hex::encode(&signature));
+    //   println!("Hashed data: {}", hex::encode(data));
+    // }
 }
