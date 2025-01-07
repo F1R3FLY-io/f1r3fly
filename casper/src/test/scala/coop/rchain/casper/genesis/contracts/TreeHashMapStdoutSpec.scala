8fd93311ed11fbdab6b7e9d2e133a8004c1fb188
steve preston <stevepreston747@gmail.com>
2025-01-06 17:24:24 -0800

    removing dup files; adding TreeHashStdOut test

---
+++ casper/src/test/scala/coop/rchain/casper/genesis/contracts/TreeHashMapStdoutSpec.scala
@@ -0,0 +1,11 @@
+package coop.rchain.casper.genesis.contracts
+import coop.rchain.casper.helper.RhoSpec
+import coop.rchain.models.NormalizerEnv
+import coop.rchain.rholang.build.CompiledRholangSource
+
+class TreeHashMapStdoutSpec
+    extends RhoSpec(
+      CompiledRholangSource("TreeHashMapStdoutTest.rho", NormalizerEnv.Empty),
+      Seq.empty,
+      GENESIS_TEST_TIMEOUT
+    )
