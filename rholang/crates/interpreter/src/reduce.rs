d09447b67aa468514adf24533c8e4459f5297eb1
Denys Bushulyak <denys_bushulyak@icloud.com>
2025-04-17 21:17:31 +0200

    wip

--- rholang/crates/interpreter/src/reduce.rs
+++ rholang/crates/interpreter/src/reduce.rs
@@ -3229,7 +3229,7 @@ impl DebruijnInterpreter {
             locally_free,
             par.exprs
                 .iter()
-                .flat_map(|expr| expr.locally_free(expr.clone(), 0))
+                .flat_map(|expr| expr.locally_free(expr.clone()))
                 .collect(),
         );
 
