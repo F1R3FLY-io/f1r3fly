83e49d0bd519560e0e0a02c50f1fcc8d158fb036
spreston8 <stevepreston747@gmail.com>
2025-05-20 15:25:03 -0700

    last_finalized_height_constraint_checker

---
+++ casper/src/rust/last_finalized_height_constraint_checker.rs
@@ -0,0 +1,38 @@
+// See casper/src/main/scala/coop/rchain/casper/LastFinalizedHeightConstraintChecker.scala
+
+use super::{
+    blocks::proposer::propose_result::CheckProposeConstraintsResult, casper::CasperSnapshot,
+    errors::CasperError, validator_identity::ValidatorIdentity,
+};
+
+pub fn check(
+    snapshot: CasperSnapshot,
+    validator_identity: ValidatorIdentity,
+) -> Result<CheckProposeConstraintsResult, CasperError> {
+    let validator = validator_identity.public_key.bytes;
+    let last_finalized_block_hash = snapshot.dag.last_finalized_block();
+    let height_constraint_threshold = snapshot
+        .on_chain_state
+        .shard_conf
+        .height_constraint_threshold;
+
+    let last_finalized_block = snapshot.dag.lookup_unsafe(&last_finalized_block_hash)?;
+    let latest_message_opt = snapshot.dag.latest_message(&validator)?;
+    let latest_message = latest_message_opt.ok_or(CasperError::Other(
+        "Last finalized height constraint checker: Validator does not have a latest message"
+            .to_string(),
+    ))?;
+
+    let height_difference = latest_message.block_number - last_finalized_block.block_number;
+
+    log::info!(
+        "Latest message is {} blocks ahead of the last finalized block",
+        height_difference
+    );
+
+    if height_difference <= height_constraint_threshold {
+        Ok(CheckProposeConstraintsResult::success())
+    } else {
+        Ok(CheckProposeConstraintsResult::too_far_ahead_of_last_finalized())
+    }
+}
