c4441e7aed15fb5469e2033bdefe38b7618a5ca4
AndriiS-DevBrother <andrii.s@devbrother.com>
2025-08-04 20:34:09 +0300

    add unit tests

--- casper/tests/engine/mod.rs
+++ casper/tests/engine/mod.rs
@@ -1,4 +1,5 @@
 pub mod approve_block_protocol_test;
+pub mod engine_cell_test;
 pub mod lfs_block_requester_effects_spec;
 pub mod lfs_block_requester_state_spec;
 pub mod lfs_state_requester_effects_spec;
