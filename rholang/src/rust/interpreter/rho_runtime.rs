554d0430269cdb5bfae04d3d722bc17051982343
nalane <nathaniel.patrick.lane@gmail.com>
2026-03-12 15:34:01 -0600

    Migrated to IPC

--- rholang/src/rust/interpreter/rho_runtime.rs
+++ rholang/src/rust/interpreter/rho_runtime.rs
@@ -994,6 +994,29 @@ fn std_rho_chroma_processes() -> Vec<Definition> {
     ]
 }
 
+fn std_swipl_processes() -> Vec<Definition> {
+    vec![
+        Definition {
+            urn: "rho:petta:compile".to_string(),
+            fixed_channel: FixedChannels::swipl_compile_petta(),
+            arity: 2,
+            body_ref: BodyRefs::SWIPL_COMPILE_PETTA,
+            handler: Box::new(|ctx| {
+                Box::new(move |args| {
+                    let ctx = ctx.clone();
+                    Box::pin(async move {
+                        ctx.system_processes
+                            .clone()
+                            .swipl_compile_petta(args)
+                            .await
+                    })
+                })
+            }),
+            remainder: None,
+        },
+    ]
+}
+
 fn dispatch_table_creator(
     space: RhoISpace,
     dispatcher: RhoDispatch,
@@ -1015,6 +1038,7 @@ fn dispatch_table_creator(
     all_processes.extend(std_rho_crypto_processes());
     all_processes.extend(std_rho_ai_processes());
     all_processes.extend(std_rho_chroma_processes());
+    all_processes.extend(std_swipl_processes());
 
     all_processes.extend(extra_system_processes.drain(..));
 
@@ -1145,6 +1169,7 @@ fn setup_maps_and_refs(
     // When OpenAI is disabled, the NoOp service handles calls gracefully.
     let rho_ai_binding = std_rho_ai_processes();
     let rho_chroma_binding = std_rho_chroma_processes();
+    let swipl_binding = std_swipl_processes();
 
     let combined_processes = system_binding
         .iter()
@@ -1152,6 +1177,7 @@ fn setup_maps_and_refs(
         .chain(rho_ai_binding.iter())
         .chain(rho_chroma_binding.iter())
         .chain(extra_system_processes.iter())
+        .chain(swipl_binding.iter())
         .collect::<Vec<&Definition>>();
 
     let mut urn_map: HashMap<_, _> = basic_processes();
--- rholang/src/rust/interpreter/swi_prolog_service.rs
+++ rholang/src/rust/interpreter/swi_prolog_service.rs
@@ -1,62 +1,34 @@
 use std::io::Write;
 use std::path::Path;
 use std::process::Command;
-use std::sync::Arc;
 
-use swipl::atom;
-use swipl::context::{ActivatedEngine, Context};
-use swipl::engine::Engine;
-use swipl::init::initialize_swipl;
-use swipl::pred;
-use swipl::term;
 use tempfile::NamedTempFile;
 
 use super::errors::InterpreterError;
 
-#[derive(Clone)]
-pub struct SwiplClient {
-    engine: Arc<Engine>
+pub fn evaluate(_swi_prolog_code: &str) -> Result<(), InterpreterError> {
+    Ok(())
 }
 
-impl SwiplClient {
-    pub fn new() -> Self {
-        let engine = Arc::new(Engine::new());
+pub fn petta_compile(metta_code: &str) -> Result<String, InterpreterError> {
+    // Write the MeTTa code to a temp file
+    let mut metta_file = NamedTempFile::new()
+        .map_err(|_| InterpreterError::SwiplError("Can't open temp file".into()))?;
+    metta_file.write(metta_code.as_bytes())
+        .map_err(|_| InterpreterError::SwiplError("Can't write MeTTa code to temp file".into()))?;
 
-        Self {
-            engine
-        }
-    }
+    // Get the path to PeTTa
+    let root = Path::new(env!("CARGO_MANIFEST_DIR")); 
+    let petta_path = root.join("../PeTTa/src/main.pl");
 
-    pub fn evaluate(&self, swi_prolog_code: &str) -> Result<(), InterpreterError> {
-        let context: Context<_> = self.engine.activate().into();
+    // Execute PeTTa
+    let output = Command::new("swipl")
+        .arg(petta_path)
+        .arg(metta_file.path())
+        .output()
+        .map_err(|_| InterpreterError::SwiplError("Can't translate MeTTa with PeTTa".into()))?;
 
-        let term = term! {context: swi_prolog_code}
-            .map_err(|_| InterpreterError::SwiplError("".into()))?;
-        let query = context.open_call(&term);
-
-        Ok(())
-    }
-
-    pub fn petta_compile(&self, metta_code: &str) -> Result<String, InterpreterError> {
-        // Write the MeTTa code to a temp file
-        let mut metta_file = NamedTempFile::new()
-            .map_err(|_| InterpreterError::SwiplError("Can't open temp file".into()))?;
-        metta_file.write(metta_code.as_bytes())
-            .map_err(|_| InterpreterError::SwiplError("Can't write MeTTa code to temp file".into()))?;
-
-        // Get the path to PeTTa
-        let root = Path::new(env!("CARGO_MANIFEST_DIR")); 
-        let petta_path = root.join("../PeTTa/src/main.pl");
-
-        // Execute PeTTa
-        let output = Command::new("swipl")
-            .arg(petta_path)
-            .arg(metta_file.path())
-            .output()
-            .map_err(|_| InterpreterError::SwiplError("Can't translate MeTTa with PeTTa".into()))?;
-
-        // Get output as string
-        String::from_utf8(output.stdout)
-            .map_err(|_| InterpreterError::SwiplError("Can't interpret PeTTa output".into()))
-    }
+    // Get output as string
+    String::from_utf8(output.stdout)
+        .map_err(|_| InterpreterError::SwiplError("Can't interpret PeTTa output".into()))
 }
